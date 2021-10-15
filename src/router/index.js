944e972001ffebfab276385d568f0f9b1f3fbaeb
prayogayogi <yogikpiter@gmail.com>
2021-10-16 01:53:06 +0700

    Finish slecing Html to Vue

---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Product from '../views/Product.vue'
+import ShoppingCart from '../views/ShoppingCart.vue'
+import Success from '../views/Success.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/product',
+    name: 'Product',
+    component: Product
+  },
+  {
+    path: '/chart',
+    name: 'Chart',
+    component: ShoppingCart
+  },
+  {
+    path: '/success',
+    name: 'Success',
+    component: Success
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Product.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="product">
+    <HeaderShayna/>
+      <!-- Breadcrumb Section Begin -->
+      <div class="breacrumb-section">
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-12 ">
+                      <div class="breadcrumb-text product-more text-left">
+                        <router-link to="/"><i class="fa fa-home"></i> Home</router-link>
+                          <span>Detail</span>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <!-- Breadcrumb Section Begin -->
+
+      <!-- Product Shop Section Begin -->
+      <section class="product-shop spad page-details">
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-12">
+                      <div class="row">
+                          <div class="col-lg-6">
+                              <div class="product-pic-zoom">
+                                  <img class="product-big-img" :src="gambarDefault" alt="" />
+                              </div>
+                              <div class="product-thumbs">
+                                  <carousel class="product-thumbs-track ps-slider" :dots="false" :items="3" :nav="false">
+                                      <div class="pt" @click="ganti([0])" :class="gambar[0] == gambarDefault ? 'active' : '' ">
+                                          <img src="img/mickey1.jpg" alt="" />
+                                      </div>
+
+                                      <div class="pt" @click="ganti([1])" :class="gambar[1] == gambarDefault ? 'active' : '' ">
+                                          <img src="img/mickey2.jpg" alt="" />
+                                      </div>
+
+                                      <div class="pt" @click="ganti([2])" :class="gambar[2] == gambarDefault ? 'active' : '' ">
+                                          <img src="img/mickey3.jpg" alt="" />
+                                      </div>
+
+                                      <div class="pt" @click="ganti([3])" :class="gambar[3] == gambarDefault ? 'active' : '' ">
+                                          <img src="img/mickey4.jpg" alt="" />
+                                      </div>
+                                  </carousel>
+                              </div>
+                          </div>
+                          <div class="col-lg-6 text-left">
+                              <div class="product-details">
+                                  <div class="pd-title">
+                                      <span>oranges</span>
+                                      <h3>Pure Pineapple</h3>
+                                  </div>
+                                  <div class="pd-desc">
+                                      <p>
+                                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error officia. Rem aperiam laborum voluptatum vel, pariatur modi hic provident eum iure natus quos non a sequi, id accusantium! Autem.
+                                      </p>
+                                      <p>
+                                          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam possimus quisquam animi, commodi, nihil voluptate nostrum neque architecto illo officiis doloremque et corrupti cupiditate voluptatibus error illum. Commodi expedita animi nulla aspernatur.
+                                          Id asperiores blanditiis, omnis repudiandae iste inventore cum, quam sint molestiae accusamus voluptates ex tempora illum sit perspiciatis. Nostrum dolor tenetur amet, illo natus magni veniam quia sit nihil dolores.
+                                          Commodi ratione distinctio harum voluptatum velit facilis voluptas animi non laudantium, id dolorem atque perferendis enim ducimus? A exercitationem recusandae aliquam quod. Itaque inventore obcaecati, unde quam
+                                          impedit praesentium veritatis quis beatae ea atque perferendis voluptates velit architecto?
+                                      </p>
+                                      <h4>$495.00</h4>
+                                  </div>
+                                  <div class="quantity">
+                                     <router-link to="/chart"> <a href="#" class="primary-btn pd-cart">Add To Cart</a> </router-link>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>
+      <!-- Product Shop Section End -->
+
+      <!-- Product Related Section Begin -->
+      <RelatedShayna/>
+      <!-- Product Related Section Begin -->
+    <FooterShayna/>
+  </div>
+</template>
+
+<script>
+import carousel from 'vue-owl-carousel'
+
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+import RelatedShayna from '@/components/RelatedShayna.vue'
+
+export default {
+  name: "Product",
+  components: {
+    HeaderShayna,
+    FooterShayna,
+    carousel,
+    RelatedShayna 
+  },
+  data:function(){
+      return {
+          gambarDefault:'img/mickey1.jpg',
+          gambar:[
+              'img/mickey1.jpg',
+              'img/mickey2.jpg',
+              'img/mickey3.jpg',
+              'img/mickey4.jpg'
+          ],
+      }
+  },
+  methods:{
+      ganti: function(el){
+         this.gambarDefault = this.gambar[el]
+      }
+  }
+};
+</script>
+
+<style scoped>
+.pt{
+  margin-right: 10px;
+}
+</style>
