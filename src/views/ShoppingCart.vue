05e70ffba1282b22bdc7536a00430bd4f66697a6
prayogayogi <yogikpiter@gmail.com>
2021-10-25 02:04:05 +0700

    Done App next Deploy to vps

--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -63,19 +63,19 @@
                                   <form>
                                       <div class="form-group">
                                           <label for="namaLengkap">Nama lengkap</label>
-                                          <input type="text" class="form-control" id="namaLengkap" aria-describedby="namaHelp" placeholder="Masukan Nama">
+                                          <input type="text" class="form-control" id="namaLengkap" aria-describedby="namaHelp" placeholder="Masukan Nama" v-model="customerInfo.name">
                                       </div>
                                       <div class="form-group">
                                           <label for="namaLengkap">Email Address</label>
-                                          <input type="email" class="form-control" id="emailAddress" aria-describedby="emailHelp" placeholder="Masukan Email">
+                                          <input type="email" class="form-control" id="emailAddress" aria-describedby="emailHelp" placeholder="Masukan Email" v-model="customerInfo.email">
                                       </div>
                                       <div class="form-group">
                                           <label for="namaLengkap">No. HP</label>
-                                          <input type="text" class="form-control" id="noHP" aria-describedby="noHPHelp" placeholder="Masukan No. HP">
+                                          <input type="text" class="form-control" id="noHP" aria-describedby="noHPHelp" placeholder="Masukan No. HP" v-model="customerInfo.number">
                                       </div>
                                       <div class="form-group">
                                           <label for="alamatLengkap">Alamat Lengkap</label>
-                                          <textarea class="form-control" id="alamatLengkap" rows="3"></textarea>
+                                          <textarea class="form-control" id="alamatLengkap" rows="3" v-model="customerInfo.address"></textarea>
                                       </div>
                                   </form>
                               </div>
@@ -88,14 +88,16 @@
                               <div class="proceed-checkout">
                                   <ul>
                                       <li class="subtotal">ID Transaction <span>#SH12000</span></li>
-                                      <li class="subtotal mt-3">Subtotal <span>$240.00</span></li>
-                                      <li class="subtotal mt-3">Pajak <span>10%</span></li>
-                                      <li class="subtotal mt-3">Total Biaya <span>$440.00</span></li>
+                                      <li class="subtotal mt-3">Subtotal <span>${{ totalHarga }}.00</span></li>
+                                      <li class="subtotal mt-3">Pajak <span>10% + ${{ ditambahPajak }}.00</span></li>
+                                      <li class="subtotal mt-3">Total Biaya <span>${{ totalBiaya }}.00</span></li>
                                       <li class="subtotal mt-3">Bank Transfer <span>Mandiri</span></li>
                                       <li class="subtotal mt-3">No. Rekening <span>2208 1996 1403</span></li>
                                       <li class="subtotal mt-3">Nama Penerima <span>Shayna</span></li>
                                   </ul>
-                                 <router-link to="/success"> <a href="success.html" class="proceed-btn">I ALREADY PAID</a></router-link>
+                                 <!-- <router-link to="/success"> -->
+                                  <a href="#" @click.prevent="checkOut()" class="proceed-btn">I ALREADY PAID</a>
+                                  <!-- </router-link> -->
                               </div>
                           </div>
                       </div>
@@ -108,7 +110,8 @@
 </template>
 
 <script>
-import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import axios from 'axios';
 
 export default {
   name: "ShoppingCard",
@@ -117,8 +120,14 @@ export default {
   },
    data(){
       return {
-          keranjangUser: []
-      }
+          keranjangUser: [],
+          customerInfo:{
+              name: '',
+              email: '',
+              number: '',
+              address: ''
+          }
+      };
   },
   methods:{
     removeItem(index){
@@ -126,6 +135,32 @@ export default {
     const parsed = JSON.stringify(this.keranjangUser);
     localStorage.setItem('keranjangUser', parsed);
   },
+  checkOut(){
+      let productId = this.keranjangUser.map(function(product){
+          return product.id
+      });
+
+      let checkoutData = {
+          'name' : this.customerInfo.name,
+          'email' : this.customerInfo.email,
+          'number' : this.customerInfo.number,
+          'address' : this.customerInfo.address,
+          'transaction_total' : this.totalBiaya,
+          'transaction_status' : 'PENDING',
+          'transaction_details' : productId
+      };
+
+     let ekskusi = axios.post("http://127.0.0.1:8000/api/checkout",checkoutData)
+      .then(() => this.$router.push("success"))
+      .catch(err => console.log(err));
+
+        if(ekskusi){
+            this.keranjangUser.splice(0)
+            const parsed = JSON.stringify(this.keranjangUser);
+            localStorage.setItem('keranjangUser', parsed);
+        }
+
+  }
   },
   mounted(){
       if (localStorage.getItem('keranjangUser')) {
@@ -135,7 +170,20 @@ export default {
         localStorage.removeItem('keranjangUser');
       }
       }
-  }
+  },
+  computed:{
+      totalHarga(){
+          return this.keranjangUser.reduce(function(items, data){
+              return items + data.price;
+          }, 0)
+      },
+      ditambahPajak(){
+          return (this.totalHarga * 10) / 100;
+      },
+      totalBiaya(){
+          return this.totalHarga + this.ditambahPajak
+      }
+  },
 };
 </script>
 
